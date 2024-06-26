3cfe707e90b250395652c1af6d75e24a7f8b4a7f
Chukwudi64 <chukwudiodoh64@gmail.com>
2024-06-26 15:23:00 +0100

    first commit

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <Header />
+      <Home />
+
+      {/* Home */}
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* Search Page */}
+      {/* Header */}
+    </div>
+  );
+}
+
+export default App;
